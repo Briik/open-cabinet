f69896c358cc0c0746e7fa9615a474e8585832a5
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-03 19:26:37 -0400

    fixed params

--- .pipeline/run-unit-tests.sh
+++ .pipeline/run-unit-tests.sh
@@ -24,8 +24,8 @@ function create_secrets {
   environment: development
   secret_key_base: d9c69d37907ea27c1970faf75661433eb8ac11e725bece21fc32ca76274c40b0bb404b09548aa7441e1f801a04f10612c1d104b5388d41c525a9012621dcae01
   db_host: $(get_db_hostname --region ${region} --stackname $(get_pipeline_property --key open_cabinet_rds_stack_name))
-  db_un: dev_db_user
-  db_pw: super_user
+  db_un: dbuser
+  db_pw: dbpassword
   un: user
   pw: password
   import_key: tQ2ILy9FhJedWF2iH09nwIKdNV7eEhMXsz4c8zef
