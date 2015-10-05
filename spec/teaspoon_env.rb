e6794e15c642b03a26e26c73290a9b078d2056cf
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-05 16:20:35 -0400

    acpt tests

--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -162,7 +162,7 @@ Teaspoon.configure do |config|
 
     # The path that the coverage should be written to - when there's an artifact to write to disk.
     # Note: Relative to `config.root`.
-  coverage.output_path = 'coverage/rcov'
+    coverage.output_path = 'coverage/rcov'
 
     # Assets to be ignored when generating coverage reports. Accepts an array of filenames or regular expressions. The
     # default excludes assets from vendor, gems and support libraries.
