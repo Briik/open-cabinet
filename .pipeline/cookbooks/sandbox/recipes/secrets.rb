3b21b250c34daf4cd1d0d5053a38dd7742006c48
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-09 15:08:56 -0400

    chef issues

--- .pipeline/cookbooks/sandbox/recipes/secrets.rb
+++ .pipeline/cookbooks/sandbox/recipes/secrets.rb
@@ -5,7 +5,7 @@ template "#{node[:myuscis][:app][:location]}/config/secrets.yml" do
               un: node[:myuscis][:app][:un],
               pw: node[:myuscis][:app][:pw],
               secret_key_base: node[:myuscis][:app][:secret_key_base],
-              db_host: node[:databases][:host],
+              db_host: node[:postgresql][:databases][:host],
               db_un: node[:myuscis][:app][:db_un],
               db_pw: node[:myuscis][:app][:db_pw],
               import_key: node[:myuscis][:app][:import_key]
