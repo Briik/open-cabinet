335aebda81653bc9096ba69c80daa7b56999aa36
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-09 16:00:42 -0400

    chef issues

--- .pipeline/cookbooks/sandbox/attributes/secrets.rb
+++ .pipeline/cookbooks/sandbox/attributes/secrets.rb
@@ -1,7 +1,7 @@
 default[:myuscis][:app][:un] = nil
 default[:myuscis][:app][:pw] = nil
 default[:myuscis][:app][:secret_key_base] = nil
-default[:postgresql][:databases][:host] = nil
+default[:myuscis][:app][:host] = nil
 default[:myuscis][:app][:db_un] = nil
 default[:myuscis][:app][:db_pw] = nil
 default[:myuscis][:app][:import_key] = nil
