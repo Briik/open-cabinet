c7bb1444bf086464af709ce597a37516049f4184
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-11 14:31:08 -0400

    env vars

--- .pipeline/cookbooks/sandbox/recipes/environment_variables.rb
+++ .pipeline/cookbooks/sandbox/recipes/environment_variables.rb
@@ -4,7 +4,7 @@ template "#{node[:myuscis][:app][:location]}/config/initializers/0_environment_v
   variables ({
               disable_ga: node[:myuscis][:app][:disable_ga],
               disable_auth: node[:myuscis][:app][:disable_auth],
-              disable_newrelic: node[:myuscis][:new_relic][:disable_newrelic]
+              disable_newrelic: node[:myuscis][:new_relic][:disable_newrelic],
               secret_key_base: node[:myuscis][:app][:secret_key]
             })
 end
