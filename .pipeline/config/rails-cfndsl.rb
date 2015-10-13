26d179e6b23dd54a3cd12c7e02536ff280300453
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-13 16:22:59 -0400

    params

--- .pipeline/config/rails-cfndsl.rb
+++ .pipeline/config/rails-cfndsl.rb
@@ -383,6 +383,7 @@ CloudFormation {
       'export database_host=\'', Ref('databaseHost'), "'\n",
       'export database_username=\'', Ref('databaseUser'), "'\n",
       'export database_password=\'', Ref('databasePass'), "'\n",
+      'export import_key=\'', "tQ2ILy9FhJedWF2iH09nwIKdNV7eEhMXsz4c8zef",
     ]
 
     signal_completion = [
