38a2c664610871ac5b9738f27fb0266d1b9c4d2a
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-14 00:33:00 -0400

    bluegreen

--- .pipeline/blue-green-deploy-dev-environment.sh
+++ .pipeline/blue-green-deploy-dev-environment.sh
@@ -3,22 +3,27 @@
 export vpc_label=${name_of_jenkins_stack/-*/}
 
 #when all pipelines have updated bundler, this can happens at the user level
-bundler config --local mirror.http://rubygems.org http://${vpc_label,,}-nexus.${domain}/nexus/content/repositories/rubygemsproxy/
-bundler config --local mirror.https://rubygems.org http://${vpc_label,,}-nexus.${domain}/nexus/content/repositories/rubygemsproxy/
+#bundler config --local mirror.http://rubygems.org http://${vpc_label,,}-nexus.${domain}/nexus/content/repositories/rubygemsproxy/
+#bundler config --local mirror.https://rubygems.org http://${vpc_label,,}-nexus.${domain}/nexus/content/repositories/rubygemsproxy/
 
 bundle install --jobs 4 \
                --gemfile=$(dirname $0)/Gemfile \
                --retry 10
 
+gem install myuscis-common-pipeline-0.0.30.gem
+
 source $(gem contents myuscis-common-pipeline | grep common-bash-functions)
 
-confirm_env_vars_available 'sdb_domain region pipeline_instance_id configStore configStorePass'
+confirm_env_vars_available 'region pipeline_instance_id configStore configStorePass'
 
 ###################################################################
 
 rails_stack_name=$(get_pipeline_property --key sandbox_rails_stack_name)
 
 target_env=$(get_pipeline_property --key targetEnv)
+
+set_inventory_parameter --parameter domain --value "development.com"
+set_inventory_parameter --parameter ${target_env}_subdomainName --value "development"
 
 subdomain_name=$(get_inventory_parameter --parameter ${target_env}_subdomainName)
 
