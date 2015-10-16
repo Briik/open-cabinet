60d1ff196a357623eaa17fdbc3a651a7206b2f51
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-16 15:33:51 -0400

    data

--- .pipeline/run-unit-tests.sh
+++ .pipeline/run-unit-tests.sh
@@ -58,6 +58,8 @@ create_secrets
 bundle exec rake db:drop
 bundle exec rake db:create
 bundle exec rake db:migrate
+bundle exec rake db:drop RAILS_ENV=test
+bundle exec rake db:create RAILS_ENV=test
 bundle exec rake db:migrate RAILS_ENV=test
 bundle exec rspec
 
