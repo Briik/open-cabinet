177a043e2cd842c8a4a9f6fa379964d7c865ccb7
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-17 14:47:52 -0400

    button color

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'can contain a cabinet' do
     cabinet = Cabinet.create!
-    User.create!(email: 'test@test.com', password: 'password', cabinet: cabinet)
+    User.create!(email: 'test1@test.com', password: 'password', cabinet: cabinet)
 
     expect(User.first.cabinet.id).to eq(cabinet.id)
   end
