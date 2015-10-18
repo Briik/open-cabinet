38518953c3da27a27eeead659a83c01d43c9c88e
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-18 09:25:51 -0400

    button color

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe User, type: :model do
     User.create!(email: 'test1@test.com', password: 'password', cabinet: cabinet)
 
     #expect(User.first.cabinet.id).to eq(cabinet.id)
-    expect(1=1)
+    expect(1.to eq 1)
   end
 end
