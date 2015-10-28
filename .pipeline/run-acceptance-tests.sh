ffb71a8f50f9c187d0f99845ec80cf34f8341f27
pattyb0y <pattyb0yrasche@gmail.com>
2015-10-28 18:04:26 -0400

    acceptance tests

--- .pipeline/run-acceptance-tests.sh
+++ .pipeline/run-acceptance-tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "app/system acceptance tests against the acceptance environment - NOT local to jenkins - placeholder"
+  times=(1 2)
 
+  for time in ${times[@]}
+  do
+    sleep 5
+  done
 exit 0
